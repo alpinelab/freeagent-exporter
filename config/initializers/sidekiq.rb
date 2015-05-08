e0c123a3569047072a3545c10705109a4af68816
Michael Baudino <michael.baudino@alpine-lab.com>
2015-05-08 14:42:16 +0200

    Add missing require in sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   username == Rails.application.secrets.sidekiq_username && password == Rails.application.secrets.sidekiq_password
 end if Rails.env.production?
