c8c9b0183ebc712d41d243b789fc10432015afb1
Yann Vaillant <yann@vaillant.im>
2015-05-25 15:54:15 +0200

    add account and year filter, show archive for one year

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,8 +1,16 @@
 class ArchivesController < ApplicationController
   def index
-    @archives = {}
-    current_user.bank_accounts.each do |account|
-      @archives[account] = account.archives.order(date: :desc)
+    @year = params[:year] || Date.today.year
+    @account = params[:account].present? ? BankAccount.find(params[:account]) : current_user.bank_accounts.first
+    @archives = Array.new(12) do |month|
+      Archive.find_or_create_by(
+        bank_account: @account,
+        date: Date.new(@year.to_i, month+1, 01)
+      )
     end
+  end
+
+  def generate
+
   end
 end
