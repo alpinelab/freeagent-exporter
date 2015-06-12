84d03d50e07c8746e5e1ed290df36f9e9d0a6f70
Yann Vaillant <yann@vaillant.im>
2015-06-12 14:49:05 +0200

    only create archives if account is tracked

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,8 +2,10 @@ class ArchivesController < ApplicationController
   def index
     @year = params[:year].to_i == 0 ? Date.today.year : params[:year].to_i
     @account = params[:account].present? ? current_user.bank_accounts.find(params[:account]) : current_user.bank_accounts.first
-    @archives = Array.new(12) do |month|
-      Archive.find_or_create_by(bank_account: @account, date: Date.new(@year, month+1, 01))
+    if @account
+      @archives = Array.new(12) do |month|
+        Archive.find_or_create_by(bank_account: @account, date: Date.new(@year, month+1, 01))
+      end
     end
   end
 
