340f48a0247a2248752bc888552517448fa7f578
Yann Vaillant <yann@vaillant.im>
2015-06-12 15:20:24 +0200

    refactoring + add validations in Archive

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,11 +1,9 @@
 class ArchivesController < ApplicationController
+  before_action :find_year, :find_account
+
   def index
-    @year = params[:year].to_i == 0 ? Date.today.year : params[:year].to_i
-    @account = params[:account].present? ? current_user.bank_accounts.find(params[:account]) : current_user.bank_accounts.first
-    if @account
-      @archives = Array.new(12) do |month|
-        Archive.find_or_create_by(bank_account: @account, date: Date.new(@year, month+1, 01))
-      end
+    @archives = Array.new(12) do |month|
+      Archive.find_or_create_by(bank_account: @account, date: Date.new(@year, month+1, 01))
     end
   end
 
@@ -14,5 +12,16 @@ class ArchivesController < ApplicationController
     archive = Archive.find(params[:id])
     ExpensesImport.perform_async(archive.id)
     redirect_to archives_path
+  end
+
+  private
+
+  def find_year
+    @year = (params[:year] || Date.today.year).to_i
+  end
+
+  def find_account
+    @account = params[:account].present? ? current_user.bank_accounts.find(params[:account]) : current_user.bank_accounts.first
+    redirect_to bank_accounts_path if @account.nil?
   end
 end
--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,3 +1,5 @@
 class Archive < ActiveRecord::Base
   belongs_to :bank_account
+
+  validates :bank_account, presence: true
 end
