60f1a96395af7332dd9c7b2802dcb619d59cc64f
Michael Baudino <michael.baudino@alpine-lab.com>
2015-06-13 14:57:17 +0200

    Refactor archives index view

--- app/helpers/archives_helper.rb
+++ app/helpers/archives_helper.rb
@@ -1,5 +1,32 @@
 module ArchivesHelper
-  def link_to_archive_or_generate(archive)
-    archive.s3_url.present? ? link_to("Download Archive", archive.s3_url) : link_to("Generate Archive", archive, method: 'PUT')
+  def archive_action_link(archive)
+    if archive.s3_url.present?
+      download_button(archive)
+    elsif archive.transactions_left_to_explain > 0
+      transactions_left_to_explain_label(archive)
+    else
+      generate_button(archive)
+    end
+  end
+
+  def download_button(archive)
+    link_to(
+      content_tag(:span, class: %w{glyphicon glyphicon-download-alt}) + "Download Archive",
+      archive.s3_url,
+      class: %w{btn btn-sm btn-success}
+    )
+  end
+
+  def transactions_left_to_explain_label(archive)
+    content_tag(:span, "#{archive.transactions_left_to_explain} transactions left to explain", class: %w{label label-warning})
+  end
+
+  def generate_button(archive)
+    link_to(
+      content_tag(:span, class: %w{glyphicon glyphicon-cog}) + "Generate Archive",
+      archive,
+      method: 'PUT',
+      class: %w{btn btn-sm btn-primary}
+    )
   end
 end
