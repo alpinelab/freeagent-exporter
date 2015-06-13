0621fa9896e4c991b364d0ef636c9b875f4253c1
Michael Baudino <michael.baudino@alpine-lab.com>
2015-06-13 15:15:28 +0200

    Add empty content to content_tags

--- app/helpers/archives_helper.rb
+++ app/helpers/archives_helper.rb
@@ -11,19 +11,19 @@ module ArchivesHelper
 
   def download_button(archive)
     link_to(
-      content_tag(:span, class: %w{glyphicon glyphicon-download-alt}).html_safe + "Download Archive",
+      content_tag(:span, "", class: %w{glyphicon glyphicon-download-alt}) + "Download Archive",
       archive.s3_url,
       class: %w{btn btn-sm btn-success}
     )
   end
 
   def transactions_left_to_explain_label(archive)
-    content_tag(:span, "#{archive.transactions_left_to_explain} transactions left to explain", class: %w{label label-warning}).html_safe
+    content_tag(:span, "#{archive.transactions_left_to_explain} transactions left to explain", class: %w{label label-warning})
   end
 
   def generate_button(archive)
     link_to(
-      content_tag(:span, class: %w{glyphicon glyphicon-cog}).html_safe + "Generate Archive",
+      content_tag(:span, "", class: %w{glyphicon glyphicon-cog}) + "Generate Archive",
       archive,
       method: 'PUT',
       class: %w{btn btn-sm btn-primary}
