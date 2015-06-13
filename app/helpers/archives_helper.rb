2d2efed11b1f45911213b95af3e0e86bb8dbefb5
Michael Baudino <michael.baudino@alpine-lab.com>
2015-06-13 15:04:13 +0200

    Add html_safe on content_tags in helpers

--- app/helpers/archives_helper.rb
+++ app/helpers/archives_helper.rb
@@ -11,19 +11,19 @@ module ArchivesHelper
 
   def download_button(archive)
     link_to(
-      content_tag(:span, class: %w{glyphicon glyphicon-download-alt}) + "Download Archive",
+      content_tag(:span, class: %w{glyphicon glyphicon-download-alt}).html_safe + "Download Archive",
       archive.s3_url,
       class: %w{btn btn-sm btn-success}
     )
   end
 
   def transactions_left_to_explain_label(archive)
-    content_tag(:span, "#{archive.transactions_left_to_explain} transactions left to explain", class: %w{label label-warning})
+    content_tag(:span, "#{archive.transactions_left_to_explain} transactions left to explain", class: %w{label label-warning}).html_safe
   end
 
   def generate_button(archive)
     link_to(
-      content_tag(:span, class: %w{glyphicon glyphicon-cog}) + "Generate Archive",
+      content_tag(:span, class: %w{glyphicon glyphicon-cog}).html_safe + "Generate Archive",
       archive,
       method: 'PUT',
       class: %w{btn btn-sm btn-primary}
