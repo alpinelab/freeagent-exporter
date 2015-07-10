0cfcc5ee813318f36b1b37f6bf53189e8ae756be
Yann Vaillant <yann@vaillant.im>
2015-07-10 17:22:33 +0200

    fix with new bank_transaction_explanations

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -24,15 +24,13 @@ class Archive < ActiveRecord::Base
   end
 
   def to_csv
-    bank_transactions = FreeAgent::BankTransaction.find_all_by_bank_account(
-      bank_account.freeagent_id,
-      { from_date: start_date, to_date: end_date }
-    )
     CSV.generate do |csv|
-      bank_transactions.each do |bank_transaction|
-        csv << [bank_transaction.id, "#{bank_transaction.id}-foo.png"]
+      FreeAgent::BankTransaction.find_all_by_bank_account(bank_account.freeagent_id, {from_date: start_date, to_date: end_date}).each do |bt|
+        FreeAgent::BankTransaction.find(bt.id).bank_transaction_explanations.each do |explanation|
+          csv << [bt.id, "#{explanation.id}-attachment.png"]  if explanation.attachment
+          csv << [bt.id, "#{explanation.id}-bill.png"] if explanation.paid_bill && explanation.paid_bill.attachment
+        end
       end
     end
-
   end
 end
