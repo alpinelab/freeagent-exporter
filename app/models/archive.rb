172a7eb4dec43eefb794ab175b2b577da334f236
Yann Vaillant <yann@vaillant.im>
2015-07-10 17:28:05 +0200

    formating

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -18,7 +18,7 @@ class Archive < ActiveRecord::Base
     CSV.generate do |csv|
       FreeAgent::BankTransaction.find_all_by_bank_account(bank_account.freeagent_id, {from_date: start_date, to_date: end_date}).each do |bt|
         FreeAgent::BankTransaction.find(bt.id).bank_transaction_explanations.each do |explanation|
-          csv << [bt.id, "#{explanation.id}-attachment.png"]  if explanation.attachment
+          csv << [bt.id, "#{explanation.id}-attachment.png"] if explanation.attachment
           csv << [bt.id, "#{explanation.id}-bill.png"] if explanation.paid_bill && explanation.paid_bill.attachment
         end
       end
