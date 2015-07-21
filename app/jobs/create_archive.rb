29801ac2d3ff24bfe281c7407f6e7799f305765e
Michael Baudino <michael.baudino@alpine-lab.com>
2015-07-21 08:41:39 +0200

    Move data feching outside of CreateArchive job

--- app/jobs/create_archive.rb
+++ app/jobs/create_archive.rb
@@ -1,4 +1,3 @@
-require 'freeagent'
 require 'zip/filesystem'
 require 'open-uri'
 require 'fileutils'
@@ -6,51 +5,40 @@ require 'fileutils'
 class CreateArchive
   include Sidekiq::Worker
 
-  attr_reader :archive_id
+  attr_reader :archive
 
   def perform(archive_id)
-    @archive_id = archive_id
-    init_freeagent
-
+    @archive = Archive.find(archive_id)
     archive.update_attributes(transactions_left_to_explain: transactions_left_to_explain)
-    return unless archive_can_be_generated?
-
-    zipfile = ArchiveGenerator.call(archive, bank_transactions, expenses, invoices)
-    ArchiveUploader.call(archive, zipfile)
+    if archive_can_be_generated?
+      zipfile = ArchiveGenerator.call(archive, bank_transactions, expenses, invoices)
+      ArchiveUploader.call(archive, zipfile)
+    end
   end
 
 private
 
-  def archive_can_be_generated?
-    transactions_left_to_explain == 0 && bank_transactions.length > 0
-  end
-
-  def init_freeagent
-    FreeAgent.environment = Rails.application.secrets.freeagent_env.to_sym
-    FreeAgent.access_details(
-      Rails.application.secrets.freeagent_id,
-      Rails.application.secrets.freeagent_secret,
-      access_token: archive.bank_account.users.first.access_token
-    )
-  end
-
-  def transactions_left_to_explain
-    @transactions_left_to_explain ||= bank_transactions.select{ |bt| bt.unexplained_amount != 0 }.count
-  end
-
-  def archive
-    @archive ||= Archive.find(@archive_id)
+  def data
+    @data ||= ArchiveDataFetcher.new(archive)
   end
 
   def bank_transactions
-    @bank_transactions ||= FreeAgent::BankTransaction.find_all_by_bank_account(archive.bank_account.freeagent_id, from_date: archive.start_date, to_date: archive.end_date)
+    @bank_transactions ||= data.bank_transactions
   end
 
   def expenses
-    @expenses ||= FreeAgent::Expense.filter(from_date: archive.start_date, to_date: archive.end_date)
+    @expenses ||= data.expenses
   end
 
   def invoices
-    @invoices ||= FreeAgent::Invoice.filter(from_date: archive.start_date, to_date: archive.end_date, nested_invoice_items: 'true')
+    @invoices ||= data.invoices
+  end
+
+  def archive_can_be_generated?
+    transactions_left_to_explain == 0 && bank_transactions.length > 0 unless bank_transactions.nil?
+  end
+
+  def transactions_left_to_explain
+    @transactions_left_to_explain ||= bank_transactions.select{ |bt| bt.unexplained_amount != 0 }.count unless bank_transactions.blank?
   end
 end
---
+++ lib/archive_data_fetcher.rb
@@ -0,0 +1,33 @@
+require 'freeagent'
+
+class ArchiveDataFetcher
+  attr_reader :archive
+
+  def initialize(archive)
+    @archive = archive
+    init_freeagent
+  end
+
+  def bank_transactions
+    FreeAgent::BankTransaction.find_all_by_bank_account(archive.bank_account.freeagent_id, from_date: archive.start_date, to_date: archive.end_date)
+  end
+
+  def expenses
+    FreeAgent::Expense.filter(from_date: archive.start_date, to_date: archive.end_date)
+  end
+
+  def invoices
+    FreeAgent::Invoice.filter(from_date: archive.start_date, to_date: archive.end_date, nested_invoice_items: 'true')
+  end
+
+private
+
+  def init_freeagent
+    FreeAgent.environment = Rails.application.secrets.freeagent_env.to_sym
+    FreeAgent.access_details(
+      Rails.application.secrets.freeagent_id,
+      Rails.application.secrets.freeagent_secret,
+      access_token: archive.bank_account.users.first.access_token
+    )
+  end
+end
