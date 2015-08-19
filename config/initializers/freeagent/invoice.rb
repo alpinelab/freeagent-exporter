3eac42ae3bae78103c42e0c131ae72c59ad2147c
Yann Vaillant <yann@vaillant.im>
2015-08-19 16:49:01 +0200

    rename method

--- config/initializers/freeagent/invoice.rb
+++ config/initializers/freeagent/invoice.rb
@@ -1,6 +1,6 @@
 module FreeAgent
   class Invoice < Resource
-    def self.filter_by_time(from, to)
+    def self.filter_by_date(from, to)
       FreeAgent::Invoice.filter(nested_invoice_items: 'true').reject { |invoice| invoice.due_on < from || invoice.due_on > to }
     end
   end
