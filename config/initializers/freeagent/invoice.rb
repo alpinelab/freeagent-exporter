7bd06721406e40b790900c1988b287bdc6d30283
Yann Vaillant <yann@vaillant.im>
2015-08-20 11:26:35 +0200

    move monkeypatch to lib folder

--- config/initializers/freeagent/invoice.rb
+++
@@ -1,7 +0,0 @@
-module FreeAgent
-  class Invoice < Resource
-    def self.filter_by_date(from, to)
-      FreeAgent::Invoice.filter(nested_invoice_items: 'true').reject { |invoice| invoice.due_on < from || invoice.due_on > to }
-    end
-  end
-end
