aa2bdebd9b4b433624b5931f9ddc7be203838727
Yann Vaillant <yann@vaillant.im>
2015-08-24 14:56:36 +0200

    stop job retries if canceld

--- app/jobs/create_archive.rb
+++ app/jobs/create_archive.rb
@@ -4,6 +4,7 @@ class CreateArchive
   attr_reader :archive
 
   def perform(archive_id)
+    return if cancelled?
     @archive = Archive.find(archive_id)
     archive.update_attributes(transactions_left_to_explain: transactions_left_to_explain)
     if transactions_left_to_explain == 0
@@ -16,9 +17,19 @@ class CreateArchive
   end
 
   def self.cancel!(archive_id)
+    workers = Sidekiq::Workers.new
+    workers.each do |process_id, thread_id, work|
+      if work['payload']['args'].first == archive_id
+        Sidekiq.redis {|c| c.setex("cancelled-#{work['payload']['jid']}", 86400, 1) }
+      end
+    end
   end
 
 private
+
+  def cancelled?
+    Sidekiq.redis {|c| c.exists("cancelled-#{jid}") }
+  end
 
   def data
     @data ||= ArchiveDataFetcher.new(archive)
